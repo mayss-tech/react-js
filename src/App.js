7622182abc9634f66b1aac3859c94ac48b3cdd52
mayssa louati <mayssaloau38@gmail.com>
2020-09-23 12:10:24 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Ex1 from "./components/ex1.js"
 import './App.css';
 import {Carousel} from "react-bootstrap"
@@ -7,44 +6,46 @@ import {Carousel} from "react-bootstrap"
 function App() {
   return (
     <div className="App">
-      <Ex1/>
-      <Carousel>
+      <Carousel> 
   <Carousel.Item>
     <img
-      className="d-block w-100"
-      src="https://www.jardiland.com/media/catalog/product/cache/small_image/240x300/beff4985b56e3afdbeabfc89641a4582/d/r/dragonnier-lemon-lime-1378958-1.jpg"
+      className="block"
+      src="https://www.deco.fr/sites/default/files/styles/card_480x275/public/2020-06/shutterstock_1473478916.jpg?itok=qMWCTu2P"
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h3>Plantes d'intérieur</h3>
+      <p>Les plantes, c’est fait, reste à trouver comment les exposer. Le bon meuble met en valeur une plante d’intérieur, 
+        sans gêner, sans perturber, tout en s’intégrant dans la déco. </p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100"
-      src="https://www.jardiland.com/media/catalog/product/cache/small_image/240x300/beff4985b56e3afdbeabfc89641a4582/d/r/dragonnier-lemon-lime-1378958-1.jpg"
-      alt="Third slide"
+      className="block"
+      src="https://fac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FFAC.2Fvar.2Ffemmeactuelle.2Fstorage.2Fimages.2Fdeco.2Ftendance-deco.2Fplantes-interieur-nouvelle-tendance-deco-46869.2F14847279-1-fre-FR.2Fplantes-d-interieur-la-nouvelle-tendance-deco.2Ejpg/850x478/quality/90/crop-from/center/plantes-d-interieur-la-nouvelle-tendance-deco.jpeg"
+      alt="second slide"
     />
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h3>Nouvelle tendance déco</h3>
+      <p>Etagères en mode forêt vierge, folie des pots suspendus, panoplie de mini-cactus ou collection de terrariums nouvelle génération,
+      le vert s’invite à l’intérieur.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100"
-      src="https://www.jardiland.com/media/catalog/product/cache/small_image/240x300/beff4985b56e3afdbeabfc89641a4582/d/r/dragonnier-lemon-lime-1378958-1.jpg"
+      className="block"
+      src="https://static.cotemaison.fr/medias_11824/w_600,h_600,c_fill,g_north/v1525420931/grande-plante-dans-un-salon_6054256.jpg"
       alt="Third slide"
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3>Salon déco</h3>
+      <p>Les plantes vertes sont une valeur sûre quand on cherche à décorer son intérieur rapidement. </p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
+<Ex1/>
     </div>
   );
 }
--- src/components/ex1.js
+++ src/components/ex1.js
@@ -3,13 +3,13 @@ class Ex1 extends Component {
     render() { 
         return ( 
             <div>
-                    <img src="https://sf1.viepratique.fr/wp-content/uploads/sites/8/2019/02/54e76d969b724d4ea7b631946c286452-273x410.jpg" alt="img1"/>
+    <img  className= "img1" src="https://sf1.viepratique.fr/wp-content/uploads/sites/8/2019/02/54e76d969b724d4ea7b631946c286452-273x410.jpg" alt="img1"/>
     <h1>Ficus Lyrata</h1>
     <button class="b" onclick="myFunction()">Voir informations</button>
     <p id="demo"></p>
             </div>
-         );
+    );
     }
 }
- 
+
 export default Ex1;
